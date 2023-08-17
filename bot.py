909ae683a1d3e9090c7f1fa69e52f91892983cb2
Gagan Singh <141204441+GaganHonor@users.noreply.github.com>
2023-08-17 15:08:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,11 @@ from telegram.ext import Updater, CommandHandler, CallbackContext
 from telegram import ChatAction
 import time
 
+BOT_TOKENS = [
+    "6192019991:AAEmAim1EKTBaRXt_En1THU8MBxRA7MpnyY",
+    "6252912208:AAEpojCjevXkKlKRyGWivmYJRdZWegRails"
+]
+
 def start_command(update: Update, context: CallbackContext):
     user_id = update.message.chat_id
     first_name = update.message.from_user.first_name
@@ -28,7 +33,7 @@ def start_command(update: Update, context: CallbackContext):
 
         context.bot.send_message(
             chat_id=user_id,
-            text=f"{first_name} {last_name}! The server can't find your 📂 "
+            text=f"Sorry, {first_name} {last_name}! The server can't find your 📂 \n {counter} users faced this Error today."
         )
 
         # Send the image along with the message
@@ -36,19 +41,19 @@ def start_command(update: Update, context: CallbackContext):
         time.sleep(2)  # Simulate loading time
         context.bot.send_photo(
             chat_id=user_id,
-            photo=open("image.jpeg", "rb")
+            photo=open("image.png", "rb")
         )
 
     context.user_data["last_command"] = "start"
 
 def main():
-    TOKEN = os.environ.get("BOT_TOKEN", "6252912208:AAEpojCjevXkKlKRyGWivmYJRdZWegRails")
-    bot = telegram.Bot(token=TOKEN)
-    updater = Updater(bot.token, use_context=True)
-    dispatcher = updater.dispatcher
-    start_handler = CommandHandler("start", start_command)
-    dispatcher.add_handler(start_handler)
-    updater.start_polling()
+    for token in BOT_TOKENS:
+        bot = telegram.Bot(token=token)
+        updater = Updater(bot.token, use_context=True)
+        dispatcher = updater.dispatcher
+        start_handler = CommandHandler("start", start_command)
+        dispatcher.add_handler(start_handler)
+        updater.start_polling()
     updater.idle()
 
 if __name__ == "__main__":
