8f5caf4d8d5dac13bb074dcd177d848681361d73
Gagan Singh <141204441+GaganHonor@users.noreply.github.com>
2023-08-17 22:18:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,36 +14,51 @@ def start_command(update: Update, context: CallbackContext):
     first_name = update.message.from_user.first_name
     last_name = update.message.from_user.last_name
 
-    if "last_command" in context.user_data and context.user_data["last_command"] == "start":
-        context.bot.send_message(
-            chat_id=user_id,
-            text=f"{first_name}, please refrain from spamming the /start command."
-        )
-    else:
-        if "counter" not in context.user_data:
-            context.user_data["counter"] = 0
+    if "counter" not in context.user_data:
+        context.user_data["counter"] = 9060
 
-        context.user_data["counter"] += 1
-        counter = context.user_data["counter"]
+    context.user_data["counter"] += 1
+    counter = context.user_data["counter"]
 
-        # Show typing animation
-        context.bot.send_chat_action(chat_id=user_id, action=ChatAction.TYPING)
-        time.sleep(2)  # Simulate loading time
+    # Show typing animation
+    context.bot.send_chat_action(chat_id=user_id, action=ChatAction.TYPING)
+    time.sleep(2)  # Simulate loading time
 
-        context.bot.send_message(
-            chat_id=user_id,
-            text=f"Sorry, {first_name} {last_name}! The server can't find your 📂"
-        )
+    context.bot.send_message(
+        chat_id=user_id,
+        text=f"Sorry, {first_name} {last_name}! The server can't find your 📂 \n {counter} users faced this Error today."
+    )
 
-        # Send the image along with the message
-        context.bot.send_chat_action(chat_id=user_id, action=ChatAction.UPLOAD_PHOTO)
-        time.sleep(2)  # Simulate loading time
-        context.bot.send_photo(
-            chat_id=user_id,
-            photo=open("image.jpeg", "rb")
-        )
+    # Send the image along with the message
+    context.bot.send_chat_action(chat_id=user_id, action=ChatAction.UPLOAD_PHOTO)
+    time.sleep(2)  # Simulate loading time
+    context.bot.send_photo(
+        chat_id=user_id,
+        photo=open("image.png", "rb")
+    )
 
-    context.user_data["last_command"] = "start"
+def set_value_command(update: Update, context: CallbackContext):
+    user_id = update.message.chat_id
+    text = update.message.text
+    input_value = text.split()[1]
+
+    # Update the value of the variable
+    context.user_data["counter"] = int(input_value)
+
+    context.bot.send_message(
+        chat_id=user_id,
+        text=f"The value has been updated to {input_value}."
+    )
+
+def restart_command(update: Update, context: CallbackContext):
+    user_id = update.message.chat_id
+
+    context.bot.send_message(
+        chat_id=user_id,
+        text="Restarting the bot..."
+    )
+
+    os.execl(sys.executable, sys.executable, *sys.argv)
 
 def main():
     for token in BOT_TOKENS:
@@ -51,7 +66,11 @@ def main():
         updater = Updater(bot.token, use_context=True)
         dispatcher = updater.dispatcher
         start_handler = CommandHandler("start", start_command)
+        set_value_handler = CommandHandler("setvalue", set_value_command)
+        restart_handler = CommandHandler("restart", restart_command)
         dispatcher.add_handler(start_handler)
+        dispatcher.add_handler(set_value_handler)
+        dispatcher.add_handler(restart_handler)
         updater.start_polling()
     updater.idle()
 
