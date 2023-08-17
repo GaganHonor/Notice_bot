7d702203a846e392fb979f256a768b356533f1fa
Gagan Singh <141204441+GaganHonor@users.noreply.github.com>
2023-08-17 14:48:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,22 +8,30 @@ def start_command(update: Update, context: CallbackContext):
     first_name = update.message.from_user.first_name
     last_name = update.message.from_user.last_name
 
-    if "counter" not in context.user_data:
-        context.user_data["counter"] = 10060
+    if "last_command" in context.user_data and context.user_data["last_command"] == "start":
+        context.bot.send_message(
+            chat_id=user_id,
+            text=f"{first_name}, please refrain from spamming the /start command."
+        )
+    else:
+        if "counter" not in context.user_data:
+            context.user_data["counter"] = 9060
 
-    context.user_data["counter"] += 1
-    counter = context.user_data["counter"]
+        context.user_data["counter"] += 1
+        counter = context.user_data["counter"]
 
-    context.bot.send_message(
-        chat_id=user_id,
-        text=f"Sorry, {first_name} {last_name}! The server can't find your 📂 \n {counter} users faced this Error today."
-    )
+        context.bot.send_message(
+            chat_id=user_id,
+            text=f"Sorry, {first_name} {last_name}! The server can't find your 📂 \n {counter} users faced this Error today."
+        )
 
-    # Send the image along with the message
-    context.bot.send_photo(
-        chat_id=user_id,
-        photo=open("image.png", "rb")
-    )
+        # Send the image along with the message
+        context.bot.send_photo(
+            chat_id=user_id,
+            photo=open("image.png", "rb")
+        )
+
+    context.user_data["last_command"] = "start"
 
 def main():
     TOKEN = os.environ.get("BOT_TOKEN", "6252912208:AAEpojCjevXkKlKRyGWivmYJRdZWegRails")
@@ -34,6 +42,6 @@ def main():
     dispatcher.add_handler(start_handler)
     updater.start_polling()
     updater.idle()
-    
+
 if __name__ == "__main__":
     main()
