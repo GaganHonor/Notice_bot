b8382fed3eb9a087a5575e6df159957669188ac0
Gagan Singh <141204441+GaganHonor@users.noreply.github.com>
2023-08-17 14:40:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,16 +2,15 @@ import os
 import telegram
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
-counter = 455
 def start_command(update: Update, context: CallbackContext):
     user_id = update.message.chat_id
     first_name = update.message.from_user.first_name
     last_name = update.message.from_user.last_name
     if "counter" not in context.user_data:
-        context.user_data["counter"] = 0
+        context.user_data["counter"] = 9060
     context.user_data["counter"] += 1
     counter = context.user_data["counter"]
-    context.bot.send_message(chat_id=user_id, text=f"Hi, {first_name} {last_name}! The server can't find your 📂 \n {counter}+{counter} users faced this error Today.")
+    context.bot.send_message(chat_id=user_id, text=f"Sorry, {first_name} {last_name}! The server can't find your 📂 \n {counter} users faced this Error today.")
     context.bot.send_photo(chat_id=user_id, photo=open("image.png", "rb"))
 
 
